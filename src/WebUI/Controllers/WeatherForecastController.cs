b7ae64e1340bee9f093e27d0a7cb7b6d48a6af39
Andrés <andres.mejia@pfsgroup.es>
2023-06-02 23:52:41 +0200

    docker

--- src/WebUI/Controllers/WeatherForecastController.cs
+++ src/WebUI/Controllers/WeatherForecastController.cs
@@ -1,11 +1,13 @@
 ﻿using FlexiStorageManager.Application.WeatherForecasts.Queries.GetWeatherForecasts;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace FlexiStorageManager.WebUI.Controllers;
 
 public class WeatherForecastController : ApiControllerBase
 {
     [HttpGet]
+    [Authorize(Policy = "AuthZPolicy")]
     public async Task<IEnumerable<WeatherForecast>> Get()
     {
         return await Mediator.Send(new GetWeatherForecastsQuery());
--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,6 +1,25 @@
 using FlexiStorageManager.Infrastructure.Persistence;
 
+// For OpenId Azure Directory
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApi(options =>
+        {
+            builder.Configuration.Bind("AzureAd", options);
+            options.TokenValidationParameters.NameClaimType = "name";
+        }, options => { builder.Configuration.Bind("AzureAd", options); });
+
+builder.Services.AddAuthorization(config =>
+    {
+        config.AddPolicy("AuthZPolicy", policyBuilder =>
+            policyBuilder.Requirements.Add(new ScopeAuthorizationRequirement() { RequiredScopesConfigurationKey = $"AzureAd:Scopes" }));
+    });
 
 // Add services to the container.
 builder.Services.AddApplicationServices();
@@ -42,7 +61,7 @@ app.UseSwaggerUi3(settings =>
 app.UseRouting();
 
 app.UseAuthentication();
-app.UseIdentityServer();
+// app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
